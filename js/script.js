15cdb8725323563d9cb8f0bcf60ad4609806d8d1
Artem <kharmas14@gmail.com>
2020-06-23 16:24:25 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict"; 
+
+console.log('arr' + " - obj");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr);
+console.log(decr);
+
+const isChecked = false,
+      isClose = false;
+
+      console.log(isChecked || !isClose);

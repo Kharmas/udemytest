0e973a28702914d0bbf499fdd27112933bc05f33
Artem <kharmas14@gmail.com>
2020-07-04 19:50:58 +0300

    add functions

--- js/script.js
+++ js/script.js
@@ -1,22 +1,90 @@
-"use strict"; 
+/*
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
 
-const numberOfFilms = +prompt("Скільки фільмів ви подивилися?","");
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+P.S. Функции вызывать не обязательно*/
 
-};
+"use strict"; 
+ 
+let numberOfFilms;
 
-const a = prompt("Один з останніх вільмів",""),
-      b = prompt("Яка його оцінка",""),
-      c = prompt("Один з останніх вільмів",""),
-      d = prompt("Яка його оцінка","");
+function start(){
+    numberOfFilms = +prompt("Скільки фільмів ви подивилися?","");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt("Скільки фільмів ви подивилися?","");
+    }
 
-console.log(personalMovieDB);
+}
+
+//start();
+
+const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+   
+
+
+
+
+
+
+function rememberMyFilms(){
+    
+    for (let i = 0; i < 2; i++){
+        const a = prompt("Один з послідніх переглянутих фільмів?",""),
+              b = prompt("Його оцінка","");
+    
+              if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+                        personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+    }
+
+}
+
+//rememberMyFilms();
+
+function detectedPersonalLevel(){
+
+    if (personalMovieDB.count < 10) {
+                alert("Перегдянуто мало фільмів");
+            }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+                alert("Ви класний глядач");
+            }else if (personalMovieDB.count >= 30) {
+                alert("Ви кіноман");
+            }else {
+                alert("Помилка");
+            }
+
+}
+
+//detectedPersonalLevel();
+
+function showMyDB(hidden){
+    if (!hidden){
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres(){
+    for (let i = 1; i <= 3; i++ ){
+            
+            personalMovieDB.genres[i-1] = prompt('Ваш улюблений жанр під номером ${i}');
+    }
+}
+
+writeYourGenres();
